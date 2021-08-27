f9732be3a29b17a2bf47483655efd604f8ea25aa
Federico Sierra <fesiga97@hotmail.com>
2021-08-26 20:23:34 -0500

    .

--- Lab1/Practica_1.X/main.c
+++ Lab1/Practica_1.X/main.c
@@ -16,61 +16,70 @@
         Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.81.7
         Device            :  PIC16F1827
         Driver Version    :  2.00
-*/
-
-/*
-    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
-    
-    Subject to your compliance with these terms, you may use Microchip software and any 
-    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
-    license terms applicable to your use of third party software (including open source software) that 
-    may accompany Microchip software.
-    
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
-    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
-    FOR A PARTICULAR PURPOSE.
-    
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
-    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
-    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
-    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
-    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
-    SOFTWARE.
-*/
+ */
 
 #include "mcc_generated_files/mcc.h"
+#include <math.h> //estan en el PATH de compilacion osea la ruta donde busca el copilador por defecto
+#include "displays.h"
+#include "definiciones.h" 
+#include "nuestrostimers.h"
 
-/*
-                         Main application
- */
-void main(void)
-{
-    // initialize the device
-    SYSTEM_Initialize();
 
-    // When using interrupts, you need to set the Global and Peripheral Interrupt Enable bits
-    // Use the following macros to:
 
-    // Enable the Global Interrupts
-    //INTERRUPT_GlobalInterruptEnable();
 
-    // Enable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptEnable();
+uint16_t valor_ADC = 0;
+adc_channel_t AN1_Channel;
+uint16_t volt_ADC = 0;
+uint8_t Pack[6];
 
-    // Disable the Global Interrupts
-    //INTERRUPT_GlobalInterruptDisable();
 
-    // Disable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptDisable();
+void main(void){
+  
+	SYSTEM_Initialize();
+	Ds_Display seven_seg;
+	Tm_Periodico timer_1ms;
+    Tm_Periodico timer_1000ms;
+	Tm_Inicie_periodico (&timer_1ms, 1000);
+	ADC_SelectChannel (AN1_Channel);
+	//Tm_Periodico MyADC1000ms;
+	Tm_Inicie_periodico (&timer_1000ms, 1000);
+	ADC_Initialize();
+	Ds_Iniciar_displays (&seven_seg, Valor_Inicial0, Valor_Inicial1, Valor_Inicial2, T_On, T_Off);
+    TMR6_StartTimer();
 
-    while (1)
-    {
-        // Add your application code
+    
+
+	while (1){
+        
+		if(TMR6_HasOverflowOccured()){
+			Tm_Procese_tiempo(&timer_1ms);
+            Ds_Procese_displays(&seven_seg);
+		}
+        
+    if(Tm_Hubo_periodico(&timer_1ms)){						
+		Tm_Baje_periodico(&timer_1ms); //bajo bandera
+        ADC_StartConversion();
+                
+        if(EUSART_is_tx_ready()){
+            Pack[0]=(seven_seg.D3 + '0');
+            Pack[1]=(',');   
+            Pack[2]=(seven_seg.D2 + '0');
+            Pack[3]=(seven_seg.D1 + '0');
+            Pack[5]=0x0D;
+            Pack[4]= '\n';
+            EUSART_Write(Pack[0]);
+            EUSART_Write(Pack[1]);
+            EUSART_Write(Pack[2]);
+            EUSART_Write(Pack[3]); 
+            EUSART_Write(Pack[4]);
+            EUSART_Write(Pack[5]);
+        }      
     }
+
+		if(ADC_IsConversionDone()){
+            valor_ADC = ADC_GetConversionResult();
+			volt_ADC = Ds_Conversor_ADC(valor_ADC);
+			Ds_Convertir_en_uni(&seven_seg, volt_ADC);
+		}
+	}
 }
-/**
- End of File
-*/
