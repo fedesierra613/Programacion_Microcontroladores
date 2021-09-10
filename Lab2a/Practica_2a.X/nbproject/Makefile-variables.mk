65e6ce85d692483e904afa1ad3cfe8333630e179
Federico Sierra <86080354+fedesierra613@users.noreply.github.com>
2021-09-10 12:07:14 -0500

    Creacion lab 2A
    Se empieza el desarrollo del lab 2a

---
+++ Lab2a/Practica_2a.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Practica_2a.X.production.null
+CND_ARTIFACT_PATH_default=dist/default/production/Practica_2a.X.production.null
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=practica2a.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/practica2a.x.tar
