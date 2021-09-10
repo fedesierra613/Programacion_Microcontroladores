92d94d30b71c7e8fddc9e231ebf8ec388651ed09
Federico Sierra <fesiga97@hotmail.com>
2021-09-09 23:53:17 -0500

    .

--- Lab1/Pooling/Practica_1.X/main.c
+++ Lab1/Pooling/Practica_1.X/main.c
@@ -1,22 +1,3 @@
-/**
-  Generated Main Source File
-
-  Company:
-    Microchip Technology Inc.
-
-  File Name:
-    main.c
-
-  Summary:
-    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
-
-  Description:
-    This header file provides implementations for driver APIs for all modules selected in the GUI.
-    Generation Information :
-        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.81.7
-        Device            :  PIC16F1827
-        Driver Version    :  2.00
- */
 
 #include "mcc_generated_files/mcc.h"
 #include <math.h> //estan en el PATH de compilacion osea la ruta donde busca el copilador por defecto
@@ -28,14 +9,14 @@
 void main(void){
   
     
-   uint16_t valor_ADC = 0;
+    uint16_t valor_ADC = 0;
     adc_channel_t AN1_Channel;
     uint16_t volt_ADC = 0;
     int Pack[6];
 	SYSTEM_Initialize();
 	Ds_Display seven_seg;
 	Tm_Periodico timer_1ms;
-  //Tm_Periodico timer_1000ms;
+    //Tm_Periodico timer_1000ms;
 	Tm_Inicie_periodico (&timer_1ms, tiempo_cont);
 	ADC_SelectChannel (AN1_Channel);
 	//Tm_Inicie_periodico (&timer_1000ms, tiempo_cont);
@@ -51,7 +32,7 @@ void main(void){
         
 		if(TMR6_HasOverflowOccured()){
 			Tm_Procese_tiempo(&timer_1ms);
-      Ds_Procese_displays(&seven_seg);
+            Ds_Procese_displays(&seven_seg);
 		}
         
         if(Tm_Hubo_periodico(&timer_1ms)){						
@@ -77,7 +58,7 @@ void main(void){
         if(ADC_IsConversionDone()){
             valor_ADC = ADC_GetConversionResult();
             volt_ADC = Ds_Conversor_ADC(valor_ADC);
-            Ds_Convertir_en_uni(&seven_seg, volt_ADC);
+            Ds_Convertir_en_uni(&seven_seg, valor_ADC);
 		}
 	};
 }
